68bebe29e63e8fd38180c75b07952efb387c5e5d
amar <amarkumar887723@gmail.com>
2024-10-05 17:12:01 +0530

    first commit

--- src/Pages/config/config.js
+++ src/Pages/config/config.js
@@ -1,4 +1,4 @@
 // config.js
-const BASE_URL = `http://13.234.124.137:4000`;
+const BASE_URL = `https://kasperinfotech.in`;
 // const BASE_URL = `https://crmbackend-mn7w.onrender.com`;
 export default BASE_URL;
