95266e1aecb003d958e9997bd61c164d7397bea2
Amar Kumar <116936028+Amar111Github@users.noreply.github.com>
2024-10-07 14:41:36 +0530

    Update config.js

--- src/Pages/config/config.js
+++ src/Pages/config/config.js
@@ -1,4 +1,4 @@
 // config.js
-const BASE_URL = `https://kasperinfotech.in`;
+const BASE_URL = `https://localhost:4000`;
 // const BASE_URL = `https://crmbackend-mn7w.onrender.com`;
 export default BASE_URL;
